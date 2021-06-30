a5e2727ffc818acd773eb691eca30f0fe314868c
Derek Jackson <d4jackson84@gmail.com>
2021-06-30 16:39:18 -0400

    working on GET functionality

--- src/cart-items.ts
+++ src/cart-items.ts
@@ -1,48 +1,50 @@
 import express, {response} from "express";
 const cartRoutes = express.Router();
 
-const maxPrice = (max: number): object[] => {
-    let maxPriceArray: any = [];
-    for (let item of cart){
-        console.log(item);
-        // item.price <= max ?  maxPriceArray.push(item) : null;
-    }
-    return maxPriceArray;
-}
+// const maxPrice = (max: number): object[] => {
+//     let maxPriceArray: object[] = [];
+//     for (let item of cart){
+//         console.log("maxPriceRuns");
+//         item.price <= max ?  maxPriceArray.push(item) : null;
+//     }
+//     console.log(max);
+//     console.log(maxPriceArray);
+//     return maxPriceArray;
+// }
 
 // cart object interface
 interface cartItem {
-    id: number,
     product: string,
     price: number,
-    quantity: number
+    quantity: number,
+    id: number,
 }
 
 // mock cart
 let cart: cartItem[] = [
     {
-        id: 1,
         product: "windshield",
         price: 375,
-        quantity: 2
+        quantity: 2,
+        id: 1,
     },
     {
-        id: 2,
         product: "tune-up",
         price: 280,
-        quantity: 1
+        quantity: 1,
+        id: 2,
     },
     {
-        id: 3,
         product: "AC drainage hose",
         price: 80,
-        quantity: 1
+        quantity: 1,
+        id: 3
     },
     {
-        id: 4,
         product: "wet/dry vacuum",
         price: 150,
-        quantity: 1
+        quantity: 1,
+        id: 4
     },             
 ]
 
@@ -50,20 +52,37 @@ let nextId: number = cart.length +1;
 
 // GET
 // get cart items
-cartRoutes.get("./cart-items", (req, res) => {
-    // let cartFilter: object = [];
+cartRoutes.get("/cart-items", (req, res) => {
+    let cartFilter: object = [];
+    let maxPriceArray: object[] = [];
+    const maximumPrice: any = req.query.maximumPrice;
     // get items, filtered by query string params
-    // if (req.query.product.cartItem.startsWith()){
-    //     maxPrice(num);
-    // }
+    // typeof parseInt(maximumPrice  === "number")   <----taken from below's if statement
+    if (req.query.maxPrice){
+        console.log("in max query");
+        // maxPrice(maximumPrice);
+        // copying maxPrice() function into here essentially
+       
+        for (let item of cart){
+            console.log("maxPriceLoopRuns");
+            item.price <= parseInt(maximumPrice) ?  maxPriceArray.push(item) : null;
+        }
+    }
+
+    console.log("get is being accessed");
     res.json(cart);
+    console.log(maximumPrice);
+    console.log(maxPriceArray);
+    return maxPriceArray;
+    
+    
 })
 
 // get cart items by :id
 cartRoutes.get("/cart-items/:id", (req, res) => {
     let foundItem = cart.find((item) => {
         return item.id === parseInt(req.params.id)
-        // ^^^^is this necc? can i just move if statement below up into this instead?
+        // ^^^^is this necc? can i just move if statement below up into this instead? ASK KYLE***
     });
     if (foundItem){
         // have item
@@ -77,13 +96,6 @@ cartRoutes.get("/cart-items/:id", (req, res) => {
 // POST 
 // add cart item to array with new id
 cartRoutes.post("/cart-items", (req, res) => {
-    // let newItem: cartItem = {
-    //     id: nextId,
-    //     product: req.body,
-    //     price: req.body,
-    //     quantity: req.body.quantity
-    // };
-    // weird but found on the internet
     const incomingItem = req.body;
     incomingItem.id = nextId;
     // increments each time new item is added to cart array
@@ -93,6 +105,44 @@ cartRoutes.post("/cart-items", (req, res) => {
     console.log(cart);
     res.status(201);
     res.json(incomingItem);
+})
+
+// PUT
+// update by id
+cartRoutes.put("/cart-items/:id", (req, res) => {
+    let itemIndex = cart.findIndex((item) => {
+        console.log(item.id);
+        return item.id === parseInt(req.params.id);
+    });
+    // found it!
+    if (itemIndex > -1){
+        console.log("found, now in if statement");
+        cart[itemIndex] = {
+            id:cart[itemIndex].id,
+            product: req.body.product,
+            price: req.body.price,
+            quantity: req.body.quantity
+        };
+        // send update cart array
+        res.status(200);
+        res.json(cart);  
+    }else {
+        res.sendStatus(204)
+    }
+    
+})
+
+
+// DELETE
+// delete item by id 
+cartRoutes.delete("/cart-items/:id", (req, res) => {
+    let itemIndex = cart.findIndex((item) => {
+        return item.id === parseInt(req.params.id);
+    });
+    // statuses "no content" : "bad request"
+    itemIndex > -1 ? cart.splice(itemIndex, 1) && res.sendStatus(204) : res.sendStatus(400);
+    console.log(cart);
+    res.status(201);
 })
 
 
